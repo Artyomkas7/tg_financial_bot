b48ace72ea11f79ee5d8c1224df1987ed2064ec2
Klimin Artem <art.klimin@mail.ru>
2025-02-06 09:31:37 +0700

    with_key

--- test_ydb.py
+++ test_ydb.py
@@ -1,13 +1,11 @@
 import ydb
 import ydb.iam
 
-ENDPOINT = "grpcs://your-database-name.ydb.yandexcloud.net:2135"
-DATABASE = "/ru-central1/b1gXXXXXXXXX/your-db"
+driver = ydb.Driver(
+    endpoint="grpcs://your-database-name.ydb.yandexcloud.net:2135",
+    database="/ru-central1/b1gXXXXXXXXX/your-db",
+    credentials=ydb.iam.ServiceAccountCredentials.from_file("authorized_key.json")
+)
 
-driver = ydb.Driver(endpoint=ENDPOINT, database=DATABASE, credentials=ydb.iam.MetadataUrlCredentials())
-
-try:
-    driver.wait(timeout=30)
-    print("✅ Успешное подключение к YDB!")
-except Exception as e:
-    print("❌ Ошибка подключения:", e)
+driver.wait(timeout=30)
+print("✅ Успешное подключение к YDB!")
