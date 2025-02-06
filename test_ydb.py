43b96792e1fb677a2203eb264fb8d280f15876c6
Klimin Artem <art.klimin@mail.ru>
2025-02-06 09:46:09 +0700

    db_correct

--- test_ydb.py
+++ test_ydb.py
@@ -3,7 +3,7 @@ import ydb.iam
 
 driver = ydb.Driver(
     endpoint="grpcs://your-database-name.ydb.yandexcloud.net:2135",
-    database="/ru-central1/b1gXXXXXXXXX/your-db",
+    database="/ru-central1/b1g86rbv28go73jml91a/etnv8re60doc9qg4iglk",
     credentials=ydb.iam.ServiceAccountCredentials.from_file("authorized_key.json")
 )
 
