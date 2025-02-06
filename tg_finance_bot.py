23b9ffe256506a1981d9c63d50cb95f26da01d7e
Klimin Artem <art.klimin@mail.ru>
2025-02-06 10:43:29 +0700

    fix_fix

--- tg_finance_bot.py
+++ tg_finance_bot.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 def create_ydb_driver():
     driver = ydb.Driver(
         endpoint="grpcs://ydb.serverless.yandexcloud.net:2135",
-        database="/ru-central1/b1g86rbv28go73jml91a/etnv8re60doc9qg4iglk",
+        database="/ru-central1/b1gXXXXXXXXX/your-db",
         credentials=ydb.iam.ServiceAccountCredentials.from_file("authorized_key.json")
     )
     driver.wait(timeout=30)
